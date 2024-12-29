a6929c7ca051287dfc6237aa8e2adff97da8ed4d
Your Name <you@example.com>
2024-12-29 20:54:35 +0800

    1

--- back_end/config.js
+++ back_end/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     my_sql: {
-        host:'172.20.140.8', //地址
+        host:'20.10.244.191', //地址
         user: 'user_galen',   // 数据库用户名
         password: 'MYSQL_ROOT_PASSWORD',   // 数据库密码
         database: 'jinitaimei'    // 数据库名称
