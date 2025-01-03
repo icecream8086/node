1057baedfb8d47555bda242f8b846febb3da75ae
LinLai <zzbsn.cn@outlook.com>
2025-01-03 14:29:53 +0800

    done

--- front_end/config.js
+++ front_end/config.js
@@ -5,5 +5,5 @@
 // //config.js
 
 // 使用export语法导出apiTarget和wsTarget变量
-export const apiTarget = 'http://localhost:3000/';
+export const apiTarget = 'http://20.10.244.191:3000/';
 export const wsTarget = 'ws://10.21.78.154:3000/';
